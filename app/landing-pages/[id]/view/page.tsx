a9427457e50e3ef9d108fda7ca5172f5c58ac5c5
OfcPeriwinkle <ajwald98@gmail.com>
2025-03-12 18:31:25 -0700

    add view page

--- app/landing-pages/[id]/view/page.tsx
+++ app/landing-pages/[id]/view/page.tsx
@@ -1,1 +1,26 @@
-export default function Page() {}
+import { fetchPageComponents } from '@/app/lib/data';
+import { DashboardButton, EditButton } from '@/app/ui/landing-pages/buttons';
+
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  const components = await fetchPageComponents(id);
+
+  const initializedPageComponents = await Promise.all(
+    components.map(async (component) => {
+      const { default: BaseComponent } = await import(
+        `@/app/ui/base-components/${component.base_component_name}`
+      );
+      return <BaseComponent key={component.id} {...JSON.parse(component.props)} />;
+    })
+  );
+
+  return (
+    <>
+      <div className="flex justify-between pb-4">
+        <DashboardButton />
+        <EditButton landingPageId={id} />
+      </div>
+      {initializedPageComponents}
+    </>
+  );
+}
